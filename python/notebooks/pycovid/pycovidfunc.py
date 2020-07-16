78d9b2c27004153c64703dad5f46e0b9fc83c5cf
psychopresley <psychopresley@gmail.com>
2020-07-16 17:41:29 -0300

    automated update 2020-07-16 17h41m

--- python/notebooks/pycovid/pycovidfunc.py
+++ python/notebooks/pycovid/pycovidfunc.py
@@ -11,7 +11,81 @@ __author__ = 'Márcio Garcia'
 __email__ = 'psycho.presley@gmail.com'
 __license__ = 'GPL'
 __status__ = 'Development'
-__date__ = '07/15/2020'
+__date__ = '07/16/2020'
+
+def commit_to_repo(repo):
+    '''
+    Function to execute the commit process to the repo object active branch.
+    It must have the gitpython module correctly imported prior to its use.
+
+    repo = git object containing the git repository data
+    '''
+    import os
+    import sys
+
+    git_dir = r"C:\Program Files\Git\cmd"
+    git_bin = os.path.join(git_dir, "git")
+
+    os.putenv("GIT_PYTHON_GIT_EXECUTABLE", git_bin)
+    os.environ.putenv("GIT_PYTHON_GIT_EXECUTABLE", git_bin)
+
+    # Making sure that it is first in PATH
+    sys.path = [git_dir] + sys.path
+    os.environ["PATH"] = os.pathsep.join([git_dir]) + os.pathsep + os.environ["PATH"]
+
+    # Only import git now, because that's when the path is checked!
+    import git
+    from datetime import datetime
+
+    # Creating commit information for repo index:
+    now_str = datetime.now()
+    now_str = datetime.strftime(now_str, format='%Y-%m-%d %Hh%Mm')
+
+    author = git.Actor('Marcio Garcia','psycho.presley@gmail.com')
+    committer = git.Actor('Marcio Garcia','psycho.presley@gmail.com')
+
+    try:
+        repo.git.add(update=True)
+        repo.index.commit("automated update {}".format(now_str))
+        origin = repo.remote(name='origin')
+        origin.push()
+        print('Commit process succesfull')
+    except:
+        print('Not able to commit. Please check git information')
+
+
+def repo_info(repo):
+    '''
+    This function returns the information of the git repository. This algorithm
+    is a direct adaptation of the one presented at:
+
+    https://www.fullstackpython.com/blog/first-steps-gitpython.html
+    '''
+    repo_path = os.getenv('GIT_REPO_PATH')
+    # Repo object used to programmatically interact with Git repositories
+
+    # check that the repository loaded correctly
+    if not repo.bare:
+        print('Repo at {} successfully loaded.'.format(repo_path))
+        print('Repo description: {}'.format(repo.description))
+        print('Repo active branch: {}'.format(repo.active_branch))
+        for remote in repo.remotes:
+            print('Remote named "{}" with URL "{}"'.format(remote, remote.url))
+        print('Last commit for repo: {}.'.format(str(repo.head.commit.hexsha)))
+
+        # take the last commit then print some information
+        commits = list(repo.iter_commits('master'))[:1]
+
+        for commit in commits:
+            print('----')
+            print('commit: {}'.format(str(commit.hexsha)))
+            print("\"{}\" by {} ({})".format(commit.summary,
+                                             commit.author.name,
+                                             commit.author.email))
+            print(str(commit.authored_datetime))
+            print(str("count: {} and size: {}".format(commit.count(),
+                                                      commit.size)))
+
 
 def raw_data_formatter(file_list,file_dir):
     import pandas as pd
