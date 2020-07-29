db80eb7ad0ca15df200e763ae3e626d84bf0b4df
psychopresley <psychopresley@gmail.com>
2020-07-29 12:33:56 -0300

    automated update 2020-07-29 12h33m

--- python/notebooks/pycovid.bat
+++ python/notebooks/pycovid.bat
@@ -1,5 +1,4 @@
 @echo on
-cd C:\Users\user\Documents\GitHub\COVID-19\python\notebooks
 call C:\Users\user\anaconda3\Scripts\activate.bat
 call C:\Users\user\anaconda3\python.exe "C:\Users\user\Documents\GitHub\COVID-19\python\notebooks\pycovid.py
 cmd /k
