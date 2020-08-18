7c2092e5e16d6f3da7e31721a9e61d96bafe28ec
psychopresley <psychopresley@gmail.com>
2020-08-18 10:35:00 -0300

    automated update 2020-08-18 10h35m

--- python/scripts/pycovid.py
+++ python/scripts/pycovid.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
 
 	flag = True # flag to indicate update
 	report=[]
-	new_db = config.loc['raw_data'].update
+	new_db = config.loc['raw_data','update']
 	if count_modified != 0 or new_db:
 		report.append('{} existing file(s) updated since last pull\n'.format(count_modified))
 		report.append('generating new database...\n')
